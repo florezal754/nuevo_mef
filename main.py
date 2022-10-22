a7bff43f4263171e4bf960c1234b2bfd4af2bf85
LauraFlórez <59582182+florezal754@users.noreply.github.com>
2022-10-22 20:01:03 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 from flask import Flask
-import gunicorn 
+from flask_cors import CORS
+
 
 #creates the Flask app object, contains data about the app and methods (object functions)
 #functions tell the app to do certain actions
 app = Flask(__name__)
+CORS(app)
+
+
 
 #starts the debugger -- if code is malformed an error will appear
 app.config["DEBUG"] = True
